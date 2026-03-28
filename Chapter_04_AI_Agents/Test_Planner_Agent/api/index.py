a5ce6f6e3f73836c22e2ee4caa53372a9b0b2f87
Pramod Dutta <pramoddutta_96@yahoo.com>
2026-03-28 10:27:53 +0530

    Refactor: Switch backend purely to Node.js/Express for bulletproof Vercel compatibility

--- Chapter_04_AI_Agents/Test_Planner_Agent/api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Allow Vercel Serverless runtime to import from root directory
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from server import app
